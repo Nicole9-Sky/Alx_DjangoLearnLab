7327f4639671aa7b258753f43352731191efeffe
Nicole9-Sky <cezeh247@gamil.com>
2025-02-24 18:08:48 +0100

    Implemented FBV and CBV with proper URL routing and templates

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf.apps.BookshelfConfig',
+    'bookshelf',
     'relationship_app',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
