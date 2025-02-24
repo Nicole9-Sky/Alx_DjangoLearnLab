252d0ffd3a6765a76b365ccaf0aa0b31104ef69b
Nicole9-Sky <cezeh247@gamil.com>
2025-02-24 17:56:35 +0100

    Implemented FBV and CBV with proper URL routing and templates

--- django_models/LibraryProject/relationship_app/models.py
+++ django_models/LibraryProject/relationship_app/models.py
@@ -1,33 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-# Author Model
-class Author(models.Model):
-    name = models.CharField(max_length=255)
+
+# Define roles
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.role}"
 
-# Book Model
+# Signal to create or update UserProfile whenever a User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+                return self.name
+        
+    class Book(models.Model):
+        title = models.CharField(max_length=200)
+        author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    
+        def __str__(self):
+            return self.title
+    
+        class Meta:
+            permissions = [
+                ("can_add_book", "Can add book"),
+                ("can_change_book", "Can change book"),
+                ("can_delete_book", "Can delete book"),
+            ]
+    
+
+
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # One-to-Many Relationship
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-# Library Model
 class Library(models.Model):
-    name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book)  # Many-to-Many Relationship
+    name = models.CharField(max_length=100, unique=True)
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
-# Librarian Model
 class Librarian(models.Model):
-    name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)  # One-to-One Relationship
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
