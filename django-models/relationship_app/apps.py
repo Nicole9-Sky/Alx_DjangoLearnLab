5802c92b11a28d2ab2f1e08c6f4335446bf69aca
Nicole9-Sky <cezeh247@gamil.com>
2025-02-24 13:14:43 +0100

    Implemented advanced Django model relationships

--- django-models/relationship_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-import relationship_app
-
-class RelationshipAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'relationship_app'
--- django-models/relationship_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Book  # Ensure this model exists
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = '__all__'
