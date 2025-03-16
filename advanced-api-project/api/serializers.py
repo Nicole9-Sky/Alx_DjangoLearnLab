fc0553238720a65929e457fc783cbc0769b459dd
Nicole9-Sky <cezeh247@gamil.com>
2025-03-16 17:17:20 +0100

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,25 +1,20 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from .models import Book, Author
 from datetime import datetime
 
 class BookSerializer(serializers.ModelSerializer):
-    
-    def validate_publication_year(self, value):
-        current_year = datetime.now().year
-        if value > current_year:
-            raise serializers.ValidationError("publication year cannot be in the future.")
-        return value  
-
     class Meta:
         model = Book
-        fields = '__all__'
-        
+        fields = ['id', 'title', 'author', 'published_date', 'isbn', 'page_count', 'cover_image']
+
+    def validate_published_date(self, value):
+        if value.year > datetime.now().year:
+            raise serializers.ValidationError("Published date cannot be in the future.")
+        return value
+
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only=True) 
-    
+    books = BookSerializer(many=True, read_only=True)
+
     class Meta:
         model = Author
-        fields = ['id', 'name', 'Book']
-        
-
-            
+        fields = ['id', 'name', 'books']
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,16 +1,26 @@
-from rest_framework import generics 
-from .models import Author, Book
-from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import generics, permissions
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
 
-# Create your views here.
-class AuthorListCreateView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class AuthorSerializer
-    
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
-    
-    
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAuthenticated()]
+        return []
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
