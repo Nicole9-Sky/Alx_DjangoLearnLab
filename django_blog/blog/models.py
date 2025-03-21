6e788280f5f0737d4f17f2c3e9e93b4299e9a31a
Nicole9-Sky <cezeh247@gamil.com>
2025-03-21 11:57:15 +0100

    adding a comment feature to the blog posts so that users will be able to read comments, and authenticated users will have the ability to post, edit, and delete their comments.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.timezone import now 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -16,3 +16,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+    
+class Comment(models.Model):
+    Post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+    
