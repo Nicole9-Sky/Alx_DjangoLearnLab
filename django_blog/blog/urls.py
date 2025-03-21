9e1a532b35fb3373c42aa1653b8938887443865f
Nicole9-Sky <cezeh247@gamil.com>
2025-03-21 18:17:09 +0100

    Added Comments in the CRUD operation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     
     # Comment URLs
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
-    path('post/<int:post_id>/comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit_comment'),
-    path('post/<int:post_id>/comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="add-comment"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="update-comment"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="delete-comment"),
 ]
     
 
